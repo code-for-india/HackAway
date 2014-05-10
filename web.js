d20ab196a99bf467f2cc31adcea9420166f67523
Ubuntu <ubuntu@ip-172-31-40-96.us-west-2.compute.internal>
2014-05-10 14:20:19 +0000

    Initial Commit

---
+++ web.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var buff = fs.readFileSync("./index.html");
+  var str = buff.toString("utf-8");
+  response.send(str);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 9000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
